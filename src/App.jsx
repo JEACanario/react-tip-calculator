28af8aa21ee619a6460cd17a47c75509e2d96727
Jose Canario <jea.canario@gmail.com>
2025-02-13 18:48:03 +0000

    new project who dis

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  const [ bill, setBill] = useState(0);
+  const [payAmount,]
+
+
+  return (
+    <>
+
+    </>
+  )
+}
